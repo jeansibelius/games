f2768d30a25854c527b568d444edfd4c9ceb854c
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2023-12-02 14:21:36 +0900

    Add test single move test suite

--- packages/tic-tac-toe/src/index.test.ts
+++ packages/tic-tac-toe/src/index.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from "vitest";
-import { initGame } from "./index.js";
+import { beforeEach, describe, expect, test } from "vitest";
+import { initGame, playerMove } from "./index.js";
 
 describe("Playing tic-tac-toe", () => {
   describe("when initialing a game", () => {
@@ -20,6 +20,41 @@ describe("Playing tic-tac-toe", () => {
 
     test("a message is shown", () => {
       const { msg } = initGame();
+      expect(msg).toBeTruthy();
+    });
+  });
+
+  describe("when making a single move", () => {
+    const currentPlayer = 1;
+    let grid: number[][];
+    let previousPlayer: number;
+    let msg: string;
+    beforeEach(() => {
+      initGame();
+      const {
+        grid: newGrid,
+        previousPlayer: newPreviousPlayer,
+        msg: newMsg,
+      } = playerMove(currentPlayer, { x: 0, y: 0 });
+      grid = newGrid;
+      previousPlayer = newPreviousPlayer;
+      msg = newMsg;
+    });
+
+    test("the grid with the new move is returned", () => {
+      const gridAfterMove = [
+        [currentPlayer, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ];
+      expect(grid).toStrictEqual(gridAfterMove);
+    });
+
+    test("the previous player value is who made the latest move", () => {
+      expect(previousPlayer).toBe(currentPlayer);
+    });
+
+    test("a message is shown", () => {
       expect(msg).toBeTruthy();
     });
   });
