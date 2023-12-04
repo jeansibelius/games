7bb6b7352cb45d7b79b7c21cc74b61bfcf031d96
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2023-12-04 22:33:51 +0900

    Add flipping algorithm & update respective test

--- packages/othello/src/index.ts
+++ packages/othello/src/index.ts
@@ -35,11 +35,7 @@ export const initGame = (): GameResponse => {
 export const playerMove = (player: Player, { x, y }: Coordinates): GameResponse => {
   try {
     setMoveToGrid(player, { x, y });
-    // TODO flip values around latest move
-    // 1. If own "color", return false
-    // 2. Continue moving in "same" direction
-    // 3. If own color, return true
-    // 4. If true, flip value to current player
+    doFlips(player, { x, y });
 
     // TODO find, if there are available moves for next player
     // If not, next player is current
@@ -54,6 +50,43 @@ const setMoveToGrid = (player: Player, { x, y }: Coordinates) => {
   if (x < 0 || x > 7 || y < 0 || y > 7) throw Error("Move outside the grid. Choose a position inside the grid.");
   if (grid[y][x]) throw Error("Tried writing over another player. Choose another position.");
   grid[y][x] = player;
+};
+
+const doFlips = (player: Player, move: Coordinates) => {
+  const directionsToCheck = [
+    { x: +0, y: +1 }, // Down
+    { x: +0, y: -1 }, // Up
+    { x: -1, y: +0 }, // Left
+    { x: +1, y: +0 }, // Right
+    { x: +1, y: +1 }, // Down-Right
+    { x: -1, y: +1 }, // Down-Left
+    { x: +1, y: -1 }, // Up-Right
+    { x: -1, y: -1 }, // Up-Left
+  ];
+  directionsToCheck.forEach((translate) => {
+    getPositionsThatCanFlip(player, move, translate).forEach(({ x, y }) => {
+      grid[y][x] = player;
+    });
+  });
+};
+
+const getPositionsThatCanFlip = (
+  player: Player,
+  thisPosition: Coordinates,
+  translate: Coordinates,
+  positionsToFlip: Coordinates[] = []
+) => {
+  const { x, y } = { x: thisPosition.x + translate.x, y: thisPosition.y + translate.y };
+  console.log("x y", x, y);
+  // Base cases
+  if (grid[y][x] === player)
+    positionsToFlip = [...positionsToFlip, ...getPositionsThatCanFlip(player, { x, y }, translate)];
+  if (x < 0 || y < 0 || x > 7 || y > 7) return positionsToFlip; // Went outside
+  if (grid[y][x] === 0) return positionsToFlip; // Reached an empty position
+  if (grid[y][x] !== player) return [{ x, y }]; // Found the enemy, flip all previous
+  console.log("positionsToFlip:", positionsToFlip);
+
+  return positionsToFlip;
 };
 
 const createResponse = (msg: string): GameResponse => ({ grid, nextPlayer, msg });
