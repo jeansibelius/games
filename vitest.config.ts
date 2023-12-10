efa43d31062631a80ac3bb2cde794b740603212b
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2023-12-10 20:46:06 +0900

    Add support for in-source testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, defaultExclude } from "vitest/config";
 
 export default defineConfig({
   test: {
+    includeSource: ["**/*.ts"],
     exclude: [...defaultExclude],
   },
 });
