3d7e1552ee960631d2ef3861736d7d5a5be3153b
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2024-01-05 12:33:34 +0900

    Update othello build

--- packages/othello/dist/index.js
+++ packages/othello/dist/index.js
@@ -238,5 +238,5 @@ const countFinal = () => {
     });
     return `${player1 === player2 ? "It's a tie" : `Winner is player ${player1 > player2 ? "1" : "2"}`}. Player 1: ${player1}. Player 2: ${player2}.`;
 };
-const createErrorResponse = (e) => createResponse(`Error: ${e.message}. Player ${nextPlayer} move again.`);
+const createErrorResponse = (e) => createResponse(`Error: ${e.message} Player ${nextPlayer} move again.`);
 const createResponse = (msg) => ({ grid, nextPlayer, nextPossibleMoves, msg });
